225f6746fe4d62c4ef16e40ee1bd645ff153f11b
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-06-06 12:34:41 +0200

    feat: agregar funcionalidad de inicio de sesión y autenticación de usuario

--- controllers/Usercontroller.js
+++ controllers/Usercontroller.js
@@ -1,5 +1,8 @@
-const { User } = require("../models/index");
+const { where } = require("sequelize");
+const { User, Token } = require("../models/index");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { jwt_secret } = require("../config/config.json");
 
 const UserController = {
   async create(req, res) {
@@ -13,6 +16,32 @@ const UserController = {
         .error(error)
         .res.status(500)
         .sed({ msg: "Error al crear el usuario", error });
+    }
+  },
+
+  async login(req, res) {
+    try {
+      const user = await User.findOne({ where: { email: req.body.email } });
+      if (!user) {
+        return res
+          .status(400)
+          .send({ msg: "Usuario o contraseña incorrectos" });
+      }
+      const isMatch = bcrypt.compare(req.body.password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .send({ msg: "Usuario o contraseña incorrectos" });
+      }
+      const token = jwt.sign({ id: user.id }, jwt_secret);
+
+      res.send({ msg: "Bienvenid@ " + user.fullName, user, token });
+      Token.create({ token, UserId: user.id });
+    } catch (error) {
+      console
+        .error(error)
+        .res.status(500)
+        .send({ msg: "Error al iniciar sesion", error });
     }
   },
 };
