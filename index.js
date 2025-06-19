caf4caaa264931983c2799b76f836ed7c34d7c65
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-06-19 13:34:08 +0200

    fix: corregir rutas de la API para que sean plurales

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use("/user", require("./routes/user"));
-app.use("/account", require("./routes/account"));
-app.use("/card", require("./routes/card"));
-app.use("/transaction", require("./routes/transaction"));
-app.use("/loan", require("./routes/loan"));
+app.use("/users", require("./routes/user"));
+app.use("/accounts", require("./routes/account"));
+app.use("/cards", require("./routes/card"));
+app.use("/transactions", require("./routes/transaction"));
+app.use("/loans", require("./routes/loan"));
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
