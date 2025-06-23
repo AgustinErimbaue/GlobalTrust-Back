8b761d98bf2f337cec96c3be4d12946cd880abf8
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-06-23 13:06:54 +0200

    feat: agregar manejo de errores y respuestas para obtener cuentas y cuenta por ID

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -1,7 +1,38 @@
 const { Account } = require("../models/index");
-const { Op, where } = require("sequelize");
 
 const AccountController = {
+  async getAccounts(req, res) {
+    try {
+      const accounts = await Account.findAll({
+        where: { UserId: req.user.id },
+      });
+      if (accounts.length === 0) {
+        return res.status(404).send({ msg: "No hay cuentas asociadas" });
+      }
+      res.status(200).send(accounts);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: "Error al obtener cuentas" });
+    }
+  },
+  async getAccountById(req, res) {
+    try {
+      const account = await Account.findByPk(req.params.id);
+
+      if (!account) {
+        return res.status(404).send({ msg: "Cuenta no encontrada" });
+      }
+
+      if (account.UserId !== req.user.id) {
+        return res.status(403).send({ msg: "No autorizado" });
+      }
+
+      res.status(200).send(account);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: "Error al obtener cuenta" });
+    }
+  },
   async createAccount(req, res) {
     try {
       let accountNumber = "";
--- routes/account.js
+++ routes/account.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { authentication } = require("../middleware/authentication");
 const AccountController = require("../controllers/AccountController");
 
+router.get("/accounts", authentication, AccountController.getAccounts);
+router.get("/account/:id", authentication, AccountController.getAccountById);
 router.post("/create-account", authentication, AccountController.createAccount);
 router.delete("/delete-account/:id", authentication, AccountController.deleteAccount);
 module.exports = router;
