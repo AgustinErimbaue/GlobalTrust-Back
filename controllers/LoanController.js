a3a3bdf954572209c659831b435d2a08d9337088
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-07-02 19:37:15 +0200

    feat: mejorar manejo de préstamos y agregar ruta para obtener préstamos por ID

--- controllers/LoanController.js
+++ controllers/LoanController.js
@@ -1,37 +1,56 @@
 const { where } = require("sequelize");
 const { Loan, User, Account } = require("../models/index");
+const { get } = require("../routes/card");
 
 const LoanController = {
-async create(req, res) {
-  try {
-    const user = await User.findOne({ where: { id: req.user.id } });
-    if (!user) {
-      return res.status(404).send({ msg: "Usted no está autorizado para esta acción" });
+  async create(req, res) {
+    try {
+      const user = await User.findOne({ where: { id: req.user.id } });
+      if (!user) {
+        return res
+          .status(404)
+          .send({ msg: "Usted no está autorizado para esta acción" });
+      }
+
+      const account = await Account.findOne({ where: { UserId: req.user.id } });
+      if (!account) {
+        return res.status(404).send({ msg: "Cuenta no encontrada" });
+      }
+
+      if (!req.body.amount || req.body.amount <= 0) {
+        return res.status(400).send({ msg: "Monto inválido" });
+      }
+
+      const loan = await Loan.create({
+        ...req.body,
+        userId: req.user.id,
+      });
+
+      account.balance = Number(account.balance) + Number(req.body.amount);
+      await account.save();
+
+      res.status(200).send({ msg: "Préstamo otorgado correctamente", loan });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: "Hubo un error al solicitar préstamo" });
     }
+  },
+  async getById(req, res) {
+    try {
+      const loans = await Loan.findAll({
+        where: { userId: req.user.id },
+      });
 
-    const account = await Account.findOne({ where: { UserId: req.user.id } });
-    if (!account) {
-      return res.status(404).send({ msg: "Cuenta no encontrada" });
+      if (loans.length === 0) {
+        return res.status(404).send({ msg: "No hay préstamos asociados" });
+      }
+
+      res.status(200).send(loans);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: "Error al obtener los préstamos" });
     }
-
-    if (!req.body.amount || req.body.amount <= 0) {
-      return res.status(400).send({ msg: "Monto inválido" });
-    }
-
-    const loan = await Loan.create({
-      ...req.body,
-      userId: req.user.id
-    });
-
-    account.balance = Number(account.balance) + Number(req.body.amount);
-    await account.save();
-
-    res.status(200).send({ msg: "Préstamo otorgado correctamente", loan });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ msg: "Hubo un error al solicitar préstamo" });
-  }
-},
+  },
 };
 
-module.exports = LoanController
+module.exports = LoanController;
--- routes/loan.js
+++ routes/loan.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { authentication } = require("../middleware/authentication");
 const LoanController = require("../controllers/LoanController");
 
+router.get("/", authentication, LoanController.getById);
 router.post("/loan-requests", authentication, LoanController.create);
 
 module.exports = router;
